b4edc33534c0e68b58b43e11f42511609e7d5d2f
AShamshin <albert.shamshin@mail.ru>
2023-01-27 14:30:05 +0300

    My TodoList button

--- src/components/TodoList/TodoLost.tsx
+++ src/components/TodoList/TodoLost.tsx
@@ -9,5 +9,27 @@ type PropsType = {
 };
 
 export function TodoList(props: PropsType) {
-  return <div>TodoList</div>;
+  function deleteTodo(id: number) {
+    let newTodo = props.todo.filter((item) => item.id !== id);
+
+    props.setTodo(newTodo);
+  }
+  function statusTodo(id: number) {
+    let newTodo = props.todo.filter((item) => {
+      if (item.id === id) {
+        item.status = !item.status;
+      }
+    });
+  }
+  return (
+    <div>
+      {props.todo.map((item) => (
+        <div key={item.id}>
+          <div>{item.title}</div>
+          <button onClick={() => deleteTodo(item.id)}>Удалить</button>
+          <button onClick={() => statusTodo(item.id)}>Закрыть/Открыть</button>
+        </div>
+      ))}
+    </div>
+  );
 }
