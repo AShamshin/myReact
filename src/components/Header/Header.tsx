0b3025f1d1e2fdae76a0594fd2b9ca3c20cd6106
AShamshin <albert.shamshin@mail.ru>
2023-01-28 12:02:02 +0300

    editTodo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import s from './Header.module.css';
+
 export function Header() {
-  return <div>Todo List</div>;
+  return <div className={s.root}>Todo List</div>;
 }
--- src/components/TodoList/TodoLost.tsx
+++ src/components/TodoList/TodoLost.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 export type TodoType = {
   id: number;
   title: string;
@@ -9,13 +11,15 @@ export type PropsType = {
 };
 
 export function TodoList(props: PropsType) {
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState('');
   function deleteTodo(id: number) {
     let newTodo = props.todo.filter((item) => item.id !== id);
 
     props.setTodo(newTodo);
   }
   function statusTodo(id: number) {
-    let newTodo = props.todo.filter((item) => {
+    let newTodo = [...props.todo].filter((item) => {
       if (item.id === id) {
         item.status = !item.status;
       }
@@ -23,13 +27,50 @@ export function TodoList(props: PropsType) {
     });
     props.setTodo(newTodo);
   }
+
+  function editTodo(id: any, title: string) {
+    setEdit(id);
+    setValue(title);
+  }
+
+  function saveTodo(id: number) {
+    let newTodo = [...props.todo].map((item) => {
+      if (item.id === id) {
+        item.title = value;
+      }
+      return item;
+    });
+
+    props.setTodo(newTodo);
+    setEdit(null);
+  }
+
   return (
     <div>
       {props.todo.map((item) => (
         <div key={item.id}>
-          <div>{item.title}</div>
-          <button onClick={() => deleteTodo(item.id)}>Удалить</button>
-          <button onClick={() => statusTodo(item.id)}>Закрыть/Открыть</button>
+          {edit === item.id ? (
+            <div>
+              <input onChange={(e) => setValue(e.target.value)} value={value} />
+            </div>
+          ) : (
+            <div>{item.title}</div>
+          )}
+          {edit === item.id ? (
+            <div>
+              <button onClick={() => saveTodo(item.id)}>Сохранить</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={() => deleteTodo(item.id)}>Удалить</button>
+              <button onClick={() => editTodo(item.id, item.title)}>
+                Редактировать
+              </button>
+              <button onClick={() => statusTodo(item.id)}>
+                Закрыть/Открыть
+              </button>
+            </div>
+          )}
         </div>
       ))}
     </div>
